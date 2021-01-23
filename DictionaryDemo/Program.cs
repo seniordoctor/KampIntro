cb026e3e53882a4b0322261f017d45c9f959b210
Recep Gevsek <202007105028@dogus.edu.tr>
2021-01-23 18:37:53 +0300

    R

---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            dictionary.Add(1,"Recep");
+            dictionary.Add(2,"Ahmet");
+            dictionary.Add(3, "Doğukan");
+            Console.WriteLine(dictionary.Count);
+
+            for (int i = 0; i < dictionary.Count; i++)
+            {
+                Console.WriteLine(dictionary.Key(i) + " : " + dictionary.Value(i));
+            }
+
+            dictionary.Add(3, "Hasan");
+            for (int i = 0; i < dictionary.Count; i++)
+            {
+                Console.WriteLine(dictionary.Key(i) + " : " + dictionary.Value(i));
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+            isimler.Add("Recep");
+            isimler.Add("Doğukan");
+            isimler.Add("Naz");
+            isimler.Add("Ahmet");
+        }
+    }
+}
