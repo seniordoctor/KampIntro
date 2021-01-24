c0d2f7ba4c582dd7b0833c3983cb3ce32d0d3c76
Recep Gevsek <202007105028@dogus.edu.tr>
2021-01-24 22:50:40 +0300

    20

---
+++ OOP3/IKrediManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    interface IKrediManager
+    {
+        //kuralları içeren kısım
+        //interface içinde olan kısımları diğer kredilerin içinde olması gerekiyor
+        void Hesapla();
+        void BiseyYap();
+    }
+}
